9ac417ed0ba82188296a34e308052ce849350831
손예섬 <sys8517@deps.kr>
2024-03-20 15:18:09 +0900

    타이핑 효과 css 완성

--- app/typing/page.tsx
+++ app/typing/page.tsx
@@ -4,12 +4,13 @@ import style from "./typing.module.scss";
 export default function TypingPage() {
   return (
     <div className={style.wrap}>
-      <div>
-        <span className={style.type_txt}>Only css - width + step()</span>
+      <div className={style.item}>
+        <span className={style.type_txt}>Only css - width + steps()</span>
+        <span className={style.hori_line}></span>
         <div className={style.container}>
           {/* https://new.atsit.in/18109/ */}
           <div className={style.text}>
-            타이핑 효과 테스트 - css 만으로 만들기 (width 조정과 step()을
+            타이핑 효과 테스트 - css 만으로 만들기 (width 조정과 steps()을
             이용했어요.)
           </div>
           {/* https://velog.io/@annari07/Typing - js */}
